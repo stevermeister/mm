3f8bba56b67b32de7883f87a6550d7eec75c9948
Stepan Suvorov <stevermeister@gmail.com>
2020-05-03 10:24:38 +0200

    center to Amsterdam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,56 @@
-import { Component, ViewChild } from '@angular/core';
-import { museums } from './museums5';
-import { MapMarker, MapInfoWindow } from '@angular/google-maps';
+import { Component, ViewChild } from "@angular/core";
+import { museums } from "./museums5";
+import { MapMarker, MapInfoWindow } from "@angular/google-maps";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   styles: [
     `
       agm-map {
         height: 100vh;
       }
-    `
+    `,
   ],
   template: `
-
-  <google-map height="400px"
-              width="750px"
-              [center]="center"
-              [zoom]="zoom">
-    <map-marker #marker
-                *ngFor="let museum of museums"
-                [position]="museum.coordinates"
-                [options]="markerOptions" (mapClick)="openInfoWindow(marker, museum)"></map-marker>
-    <map-info-window>{{infoContent}}</map-info-window>
-  </google-map>
-  `
+    <google-map height="400px" width="750px" [center]="center" [zoom]="zoom">
+      <map-marker
+        #marker
+        *ngFor="let museum of museums"
+        [position]="museum.coordinates"
+        [options]="markerOptions"
+        (mapClick)="openInfoWindow(marker, museum)"
+      ></map-marker>
+      <map-info-window>{{ infoContent }}</map-info-window>
+    </google-map>
+  `,
 })
 export class AppComponent {
-
   @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
   center;
   zoom: number = 8;
-  markerOptions = {draggable: false};
+  markerOptions = { draggable: false };
   museums = [];
   cityMuseums = {};
   cities = new Set();
-  infoContent = ''
+  infoContent = "";
 
   constructor() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position: Position) => {
-          if (position) {
-            console.log(position);
-            this.center = {lat: position.coords.latitude, lng: position.coords.longitude};
-          }
-        },
-        (error: PositionError) => console.log(error)
-      );
-    } else {
-      alert('Geolocation is not supported by this browser.');
-    }
+    this.center = { lat: 52.368456, lng: 4.8934336 }; //Amsterdam;
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(
+    //     (position: Position) => {
+    //       if (position) {
+    //         console.log(position);
+    //         this.center = {lat: position.coords.latitude, lng: position.coords.longitude};
+    //       }
+    //     },
+    //     (error: PositionError) => console.log(error)
+    //   );
+    // } else {
+    //   alert('Geolocation is not supported by this browser.');
+    // }
 
-    setTimeout(_ => {
+    setTimeout((_) => {
       console.log(JSON.stringify(Array.from(this.cities)));
       console.log(museums.length);
       this.museums = museums;
@@ -59,8 +58,7 @@ export class AppComponent {
   }
 
   openInfoWindow(marker: MapMarker, museum) {
-    this.infoContent = museum.name
+    this.infoContent = museum.name;
     this.infoWindow.open(marker);
   }
-
 }
